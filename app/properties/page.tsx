1b8a7a800f3310d5a67ecfc03dd9bac30179c7c7
riaars <riaratnasari@outlook.com>
2025-03-26 15:56:17 +0100

    add info box

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
 import React from "react";
 
 const PropertiesPage = () => {
-  console.log("hello");
-  return (
-    <div>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href={"/"}>Back to home</Link>
-    </div>
-  );
+  return <div>Properties Page</div>;
 };
 
 export default PropertiesPage;
