96de18b62667f5382c22c334a0e076289deab7e7
riaars <riaratnasari@outlook.com>
2025-03-28 10:27:41 +0100

    enhancing property card styling

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { FaMoneyBill } from "react-icons/fa";
+import { FaMoneyBill, FaBath, FaBed, FaRulerCombined } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
-import { FaBath } from "react-icons/fa";
-import { FaRulerCombined } from "react-icons/fa";
-import { FaBed } from "react-icons/fa";
+import Image from "next/image";
 
 interface Rates {
   nightly: number;
@@ -15,7 +13,7 @@ interface Location {
   city: string;
   state: string;
 }
-export interface Property {
+interface Property {
   type: string;
   name: string;
   beds: number;
@@ -23,17 +21,31 @@ export interface Property {
   square_feet: number;
   rates: Rates;
   location: Location;
+  images: string[];
 }
 
 interface PropertyCardProps {
   property: Property;
 }
 const PropertyCard = ({ property }: PropertyCardProps) => {
+  const getRateDisplay = () => {
+    const { rates } = property;
+    if (rates.monthly) {
+      return `${rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      return `${rates.weekly.toLocaleString()}/wk`;
+    } else if (rates.nightly) {
+      return `${rates.nightly.toLocaleString()}/night`;
+    }
+  };
   return (
     <div className="rounded-xl shadow-md relative">
-      <img
-        src="images/properties/a1.jpg"
+      <Image
+        src={`/images/properties/${property.images[0]}`}
         alt=""
+        height={0}
+        width={0}
+        sizes={"100vw"}
         className="w-full h-auto rounded-t-xl"
       />
       <div className="p-4">
@@ -41,39 +53,51 @@ const PropertyCard = ({ property }: PropertyCardProps) => {
           <div className="text-gray-600">{property.type}</div>
           <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
-        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right"></h3>
+        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
+          ${getRateDisplay()}
+        </h3>
 
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <FaBed /> {property.beds}
+            <FaBed className="inline mr-2" /> {property.beds}
             <span className="md:hidden lg:inline">Beds</span>
           </p>
           <p>
-            <FaBath /> {property.baths}
+            <FaBath className="inline mr-2" /> {property.baths}
             <span className="md:hidden lg:inline">Baths</span>
           </p>
           <p>
-            <FaRulerCombined />
+            <FaRulerCombined className="inline mr-2" />
             {property.square_feet}{" "}
             <span className="md:hidden lg:inline">sqft</span>
           </p>
         </div>
 
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
-          <p>
-            <FaMoneyBill />
-            Weekly
-          </p>
-          <p>
-            <FaMoneyBill />
-          </p>
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Nightly
+            </p>
+          )}
+
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Weekly
+            </p>
+          )}
+
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Monthly
+            </p>
+          )}
         </div>
 
         <div className="border border-gray-100 mb-5"></div>
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-            <FaLocationDot className="text-lg text-orange-700" />
+            <FaLocationDot className="text-lg text-orange-700 mt-1" />
             <span className="text-orange-700">
               {property.location.city} {property.location.state}
             </span>
