4ee6efb95ec2b4563e158903ae77f275e3fe7b1d
riaars <riaratnasari@outlook.com>
2025-04-03 13:50:52 +0200

    connect properties to db

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import properties from "@/properties.json";
 import PropertyCard from "@/components/PropertyCard";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
   return (
     <div className="container-xl lg:container m-auto px-4 py-6">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {properties.map((property: any) => (
+        {properties?.map((property: any) => (
           <PropertyCard key={property._id} property={property} />
         ))}
       </div>
