7dad9488d0b8f01d4010cb3bb2e6b31274041c13
riaars <riaratnasari@outlook.com>
2025-04-12 22:44:36 +0200

    adding bookmarks

---
+++ app/api/bookmarks/route.ts
@@ -0,0 +1,52 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+export const POST = async (request: Request) => {
+  try {
+    await connectDB();
+    const { propertyId } = await request.json();
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+    const { userId } = sessionUser;
+
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    //find user in db
+
+    //check if property is bookmarked
+    let isBookmarked = user?.bookmarks.includes(propertyId);
+    let message = "";
+    if (isBookmarked) {
+      //remove property from bookmarks if already bookmarked
+      user.bookmarks = user.bookmarks.filter((id) => id !== propertyId);
+      message = "Property removed from bookmarks";
+      isBookmarked = false;
+    } else {
+      user.bookmarks.push(propertyId);
+      message = "Property added to bookmarks";
+      isBookmarked = true;
+    }
+
+    await user.save();
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
---
+++ components/BookmarkButton.tsx
@@ -0,0 +1,87 @@
+"use client";
+import React from "react";
+import { FaBookmark } from "react-icons/fa6";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { toast } from "react-toastify";
+const BookmarkButton = ({ property }: any) => {
+  const { data: session } = useSession();
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const userId = session?.user?.id;
+
+  const handleClick = async () => {
+    if (!userId) {
+      toast.error("Please log in to bookmark properties.");
+      return;
+    }
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_DOMAIN}/bookmarks`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ propertyId: property._id }),
+        }
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setIsBookmarked(data.isBookmarked);
+        toast.success(data.message);
+      } else {
+        toast.error(data.message || "Something went wrong");
+      }
+    } catch (error) {
+      console.error("Error bookmarking property:", error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    const checkBookmarkStatus = async () => {
+      if (!userId) return;
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_DOMAIN}/bookmarks/check`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ propertyId: property._id }),
+          }
+        );
+        const data = await res.json();
+        if (res.ok) {
+          setIsBookmarked(data.isBookmarked);
+        } else {
+          toast.error(data.message || "Something went wrong");
+        }
+      } catch (error) {
+        console.error("Error checking bookmark status:", error);
+        toast.error("Something went wrong");
+      }
+    };
+    checkBookmarkStatus();
+  }, [userId, property._id]);
+
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
+    <button
+      onClick={handleClick}
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Bookmark Property
+    </button>
+  );
+};
+
+export default BookmarkButton;
