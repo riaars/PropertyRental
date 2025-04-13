187f050af347db3f65738d36eac1dd20fae08a1f
riaars <riaratnasari@outlook.com>
2025-04-13 23:36:07 +0200

    add search form component

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropertyCard from "@/components/PropertyCard";
 import { fetchProperties } from "@/utils/requests";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
@@ -9,17 +10,26 @@ const PropertiesPage = async () => {
   );
 
   return (
-    <div className="container-xl lg:container m-auto px-4 py-6">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {properties.length === 0 ? (
-          <p>No available properties</p>
-        ) : (
-          properties?.map((property: any) => (
-            <PropertyCard key={property._id} property={property} />
-          ))
-        )}
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flec-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <h1 className="text-2xl mb-4">Search Results</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.length === 0 ? (
+            <p>No search results found</p>
+          ) : (
+            properties?.map((property: any) => (
+              <PropertyCard key={property._id} property={property} />
+            ))
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/properties/search-results/page.tsx
+++ app/properties/search-results/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { FaCircleArrowLeft } from "react-icons/fa6";
 import Spinner from "@/components/Spinner";
 import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
@@ -47,27 +48,36 @@ const SearchResultsPage = () => {
     fetchSearchResults();
   }, [location, propertyType]);
 
-  return loading ? (
-    <Spinner loading={loading} />
-  ) : (
-    <div className="container-xl lg:container m-auto px-4 py-6">
-      <Link
-        href={"/properties"}
-        className="flex items-center text-blue-500 hover:underline mb-3"
-      >
-        <FaCircleArrowLeft className="mb-1 mr-2" /> Back to properties
-      </Link>
-      <h1 className="text-2xl mb-4">Search Results</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {properties.length === 0 ? (
-          <p>No search results found</p>
-        ) : (
-          properties?.map((property: any) => (
-            <PropertyCard key={property._id} property={property} />
-          ))
-        )}
-      </div>
-    </div>
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flec-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <Link
+            href={"/properties"}
+            className="flex items-center text-blue-500 hover:underline mb-3"
+          >
+            <FaCircleArrowLeft className="mb-1 mr-2" /> Back to properties
+          </Link>
+          <h1 className="text-2xl mb-4">Search Results</h1>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.length === 0 ? (
+              <p>No search results found</p>
+            ) : (
+              properties?.map((property: any) => (
+                <PropertyCard key={property._id} property={property} />
+              ))
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
