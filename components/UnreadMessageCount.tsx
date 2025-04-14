4c4fd99903781128a0081306202f127cebff7d0a
riaars <riaratnasari@outlook.com>
2025-04-14 16:41:05 +0200

    add unread count on navbar

---
+++ components/UnreadMessageCount.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+
+const UnreadMessageCount = ({ session }: any) => {
+  const [unreadCount, setUnreadCount] = useState(0);
+
+  useEffect(() => {
+    if (!session) return;
+    const fetchUnreadMessages = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_DOMAIN}/messages/unread-count`,
+          {
+            method: "GET",
+          }
+        );
+        if (res.status === 200) {
+          const data = await res.json();
+          setUnreadCount(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUnreadMessages();
+  }, [session]);
+
+  return (
+    unreadCount > 0 && (
+      <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+        {unreadCount}
+      </span>
+    )
+  );
+};
+
+export default UnreadMessageCount;
